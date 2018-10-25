d20c8fcea293322246effa4591481154d502a861
Dylan <dylanmccarthy@Dylans-MacBook-Pro.local>
2018-10-24 17:09:37 -0700

    entire bar rendering with color changes onclick and dropdown menu for more

--- client/src/components/MoreMenu.jsx
+++ client/src/components/MoreMenu.jsx
@@ -12,33 +12,33 @@ class MoreMenu extends Component {
     
     this.showMenu = this.showMenu.bind(this);
     this.closeMenu = this.closeMenu.bind(this);
-    this.toggleMoreClass
+    // this.toggleMoreClass = this.toggleMoreClass.bind(this)
   }
 
-  toggleMoreClass() {
-    this.setState({
-        moreClicked: !(this.state.moreClicked)
-    })
-    if (this.state.moreClicked === true) {
-        this.setState({
-            bgMoreColor: 'orange',
+//   toggleMoreClass() {
+//     this.setState({
+//         moreClicked: !(this.state.moreClicked)
+//     })
+
+
+//     if (this.state.moreClicked === true) {
+//         this.setState({
+//             bgMoreColor: 'orange',
             
-        })
-    } else {
-        this.setState({
-            bgMoreColor: 'white',
+//         })
+//     } else {
+//         this.setState({
+//             bgMoreColor: 'white',
         
-        })
-    }
-}
+//         })
+//     }
+// }
   
   showMenu(event) {
     event.preventDefault();
-    this.toggleMoreClass()
     this.setState({ 
         showMenu: true,
-        moreClicked: !(this.state.moreClicked) 
-    
+        bgMoreColor: 'orange'
     }, () => {
       document.addEventListener('click', this.closeMenu);
     });
@@ -48,10 +48,11 @@ class MoreMenu extends Component {
     
     if (!this.dropdownMenu.contains(event.target)) {
       
-      this.setState({ showMenu: false }, () => {
+      this.setState({ showMenu: false,
+        bgMoreColor: 'white'
+    }, () => {
         document.removeEventListener('click', this.closeMenu);
       });  
-      
     }
   }
 
@@ -59,7 +60,7 @@ class MoreMenu extends Component {
     return (
       <div>
           <span>
-        <button style={{backgroundColor:this.state.bgMoreColor}} onClick={this.showMenu}>
+        <button style= {{backgroundColor:this.state.bgMoreColor}} onClick={this.showMenu}>
           ...More
         </button>
         
